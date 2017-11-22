22ff6112e625675e637d8180247de37af4566192
Martín Alegría <A01022216@itesm.mx>
2017-11-21 23:43:57 -0600

    Commit 4: Everything done!

--- TablasVerdad.java
+++ TablasVerdad.java
@@ -9,10 +9,7 @@ import java.util.*;
 
 public class TablasVerdad {
 
-    /**
-    null, "\tWelcome to PiggyBank,", "PiggyBank", JOptionPane.INFORMATION_MESSAGE);
-    showInputDialog(null, "\tType your name here:", "New User", JOptionPane.INFORMATION_MESSAGE); 
-*/
+
     public static void main(String[] args) {
         
        JOptionPane ventana = new JOptionPane();
@@ -50,50 +47,36 @@ public class TablasVerdad {
                 binario = '0'+ binario;
             }//while
 
-            tabla += binario+ "\n"; ///ESTA ES LA TABLA *******************************************************************************
+            tabla += binario+ "  | \n"; ///ESTA ES LA TABLA *******************************************************************************
         }//for
 
-        String[] comp = new String[vars.length + 6];
-        String[] ops = {"&","|","!","->","<->","(",")"};
 
-        for(int i = 0; i<comp.length; i++){
+        boolean opsCheck = true;
+        String funcion = "";
 
-            if(i<vars.length){
-                comp[i] = vars[i];
-            }//if
-            else{
-                comp[i] = ops[i-vars.length];
+        do {
+
+            funcion = ventana.showInputDialog(null, "Escribe tu función utilizando tus variables " + "\n " + variables + "\n Y utilizando operadores binarios y paréntesis \n AND = & \n OR = | \n NOT = ! \n IF-THEN = -> \n IFF = <->", " Tablas de Verdad", JOptionPane.INFORMATION_MESSAGE);
+
+            if(!funcion.matches("[&|!-><->()" + variables + "]+"))
+            {
+                opsCheck = false;
+            }else {
+                opsCheck = true;
             }
 
-        }//for
 
-        String funcion = ventana.showInputDialog(null, "Escribe tu función utilizando tus variables " + "\n " + variables + "\n Y utilizando operadores binarios y paréntesis \n AND = & \n OR = | \n NOT = ! \n IF-THEN = -> \n IFF = <->" , " Tablas de Verdad", JOptionPane.INFORMATION_MESSAGE);
-
-        boolean[] check = new boolean[vars.length];
-        boolean  varCheck = true;
-
-        for(int i = 0; i<vars.length; i++){
-            if(funcion.contains(vars[i])){
-                check[i] = true;
-            }else{
-                check[i] = false;
+            if(!opsCheck){
+                ventana.showMessageDialog(null, "Introdujiste un simbolo no valido o no todas las variables, intenta de nuevo", "Tablas de Verdad", JOptionPane.ERROR_MESSAGE);
             }
 
-        }//ofr
+        }while(!opsCheck);
 
-        for(int i=0; i<check.length; i++){
+        String tablaF = variables + "  | \t " + funcion + "\n";
+        tablaF += "---------------------------------------- \n";
+        tablaF += tabla;
 
-            if(check[i]){
-                varCheck = true;
-            }else{
-                varCheck = false;
-                break;
-            }
-
-        }//for
-
-        if(varCheck){
-            System.out.println("YUP");} else { System.out.println("NAH");}
+        ventana.showMessageDialog(null, "Esta es tu tabla: \n" + tablaF , "Tablas de Verdad", JOptionPane.INFORMATION_MESSAGE);
 
 
     }//main
